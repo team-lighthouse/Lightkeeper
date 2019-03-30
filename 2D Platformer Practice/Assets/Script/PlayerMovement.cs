1c3c33447adab91293e6f72d3de5e80986293934
SEO HEE WON <gmldnjs526@naver.com>
2019-03-30 18:43:40 +0900

    Initial Commit
    연습 및 데모 구현용 프로젝트, .gitignore, README.md

---
+++ 2D Platformer Practice/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movePower;
+    public float jumpPower;
+    public float jumpTimeLimit;
+
+    Rigidbody2D rigid;
+    new SpriteRenderer renderer;
+    Animator animator;
+
+    Vector3 movement;
+    bool isJumping = false;
+    bool onPlatform = false;
+    float jumpTime;
+
+    void Start()
+    {
+        movePower = 2f;
+        jumpPower = 200f;
+        jumpTimeLimit = 0.32f;
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+        renderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+        animator = gameObject.GetComponentInChildren<Animator>();
+    }
+
+
+    void Update()
+    {
+        animator.SetFloat("VelocityY", rigid.velocity.y);
+
+        if (onPlatform)
+        {
+            animator.SetBool("OnPlatform", true);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                isJumping = true;
+                jumpTime = 0;
+            }
+        }
+        else
+        {
+            animator.SetBool("OnPlatform", false);
+        }
+
+        Debug.Log(onPlatform);
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+        bool isWalking = false;
+        
+        if (Input.GetAxisRaw ("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+            renderer.flipX = true;
+            isWalking = true;
+        }
+        else if (Input.GetAxisRaw ("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+            renderer.flipX = false;
+            isWalking = true;
+        }
+
+        animator.SetBool("Walk", isWalking);
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+        {
+            return;
+        }
+        else
+        {
+            rigid.velocity = Vector2.zero;
+
+            Vector2 jumpVelocity = new Vector2(0, jumpPower);
+            
+            rigid.AddForce(jumpVelocity, ForceMode2D.Force);
+            jumpTime += Time.deltaTime;
+
+            if (!Input.GetButton("Jump") || jumpTime > jumpTimeLimit)
+            {
+                rigid.AddForce((-0.3f) * jumpVelocity, ForceMode2D.Force);
+                isJumping = false;
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        onPlatform = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        onPlatform = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# lighthouse
+2019년 1학기 게임의 이해 프로젝트 'lightkeeper'
+
+### 참고 링크
+- [유니티 강좌] 2D 플랫포머 기초  
+http://blog.naver.com/gold_metal/220882800191
+- 2D Animation in Unity (Tutorial)  
+https://www.youtube.com/watch?v=hkaysu1Z-N8
+- [유니티 강좌] 라이프 사이클  
+https://blog.naver.com/gold_metal/221428574506
+- Replace white or single-color background with transparent online  
+https://www.imgonline.com.ua/eng/replace-white-background-with-transparent.php
